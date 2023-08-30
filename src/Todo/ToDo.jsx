7cde5bcfb5a44eb8b6efe7c26fdf9cfca3e25927
Heloisa S. Rios <131817283+heloisasatira@users.noreply.github.com>
2023-08-30 20:08:35 -0300

    Update ToDo.jsx

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -7,16 +7,16 @@ export default function ToDo() {
 
     const [lista, setLista] = useState([]);//definir que atividade será um array
 
-    const [idCount, setIdCount] = useState(1); // Estado para contar os ids
+    const [idCount, setIdCount] = useState(1); // "estado" para contar os ids
 
     const salvar = (e) => {
-        e.preventDefault();//previnir a ação de enviar o formulário
+        e.preventDefault();// previnir a ação de enviar o formulário
         setLista([...lista, {
-            id: idCount, // Atribuir o id atual
+            id: idCount, // conceder o id atual
             atividade: atividade
         }]);
-        setAtividade(""); // Limpar o campo de atividade
-        setIdCount(idCount + 1); // Incrementar o contador de ids
+        setAtividade(""); // limpar o campo de atividade
+        setIdCount(idCount + 1); // adicionar o contador de id
     }
 
     return (
